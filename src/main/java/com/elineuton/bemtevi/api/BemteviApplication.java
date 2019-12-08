87134962b9b3aa2cd1c80d666861f733f8cb96b2
Elineuton Pinheiro <elineuton.ps@gmail.com>
2019-12-08 17:07:12 -0400

    Criando a operação de instanciação

--- src/main/java/com/elineuton/bemtevi/api/BemteviApplication.java
+++ src/main/java/com/elineuton/bemtevi/api/BemteviApplication.java
@@ -1,13 +1,34 @@
 package com.elineuton.bemtevi.api;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BemteviApplication {
+import com.elineuton.bemtevi.api.domain.Instituicao;
+import com.elineuton.bemtevi.api.repositories.InstituicaoRepository;
 
+@SpringBootApplication
+public class BemteviApplication implements CommandLineRunner{
+
+	@Autowired
+	InstituicaoRepository repo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BemteviApplication.class, args);
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// Instancia os objetos quando o programa inicia
+		
+		Instituicao i1 = new Instituicao(null, "Vovó Francisca");
+		Instituicao i2 = new Instituicao(null, "Vovó Ataíde");
+		
+		repo.saveAll(Arrays.asList(i1, i2));
 		
 	}
 	
--- src/main/java/com/elineuton/bemtevi/api/resources/InstituicaoResource.java
+++ src/main/java/com/elineuton/bemtevi/api/resources/InstituicaoResource.java
@@ -1,6 +1,5 @@
 package com.elineuton.bemtevi.api.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
