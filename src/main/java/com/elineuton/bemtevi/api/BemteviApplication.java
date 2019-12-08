083863f9a97f26fb83e2f51cd0bb7187a134d04d
Elineuton Pinheiro <elineuton.ps@gmail.com>
2019-12-08 11:07:17 -0400

    Projeto criado

---
+++ src/main/java/com/elineuton/bemtevi/api/BemteviApplication.java
@@ -0,0 +1,17 @@
+package com.elineuton.bemtevi.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BemteviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BemteviApplication.class, args);
+		
+		System.out.println("Pronto!");
+	}
+	
+	
+
+}
---
+++ src/test/java/com/elineuton/bemtevi/BemteviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elineuton.bemtevi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BemteviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
