4b9f32db2c3f898fefe6057c9b0227676f132c4d
Elineuton Pinheiro <elineuton.ps@gmail.com>
2019-12-24 17:12:48 -0400

    Criando, atualizando e deletando Instituição com métodos HTTP: POST, PUT e DELETE

--- src/main/java/com/elineuton/bemtevi/api/domain/services/InstituicaoService.java
+++ src/main/java/com/elineuton/bemtevi/api/domain/services/InstituicaoService.java
@@ -3,7 +3,9 @@ package com.elineuton.bemtevi.api.domain.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.elineuton.bemtevi.api.domain.Instituicao;
@@ -25,6 +27,26 @@ public class InstituicaoService {
 		Optional<Instituicao> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " 
 		+ id + ", Tipo: " + Instituicao.class.getName()));
+	}
+	
+	public Instituicao inserir(Instituicao obj) {
+		Instituicao objSalvo = repo.save(obj);
+		return objSalvo;
+	}
+	
+	public Instituicao atualizar(Instituicao obj, Integer id) {
+		Instituicao objSalvo = repo.findById(id).get();
+		
+		if(objSalvo == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		
+		BeanUtils.copyProperties(obj, objSalvo, "id");
+		return repo.save(objSalvo);
+	}
+	
+	public void remover(Integer id) {
+		repo.deleteById(id);
 	}
 
 }
