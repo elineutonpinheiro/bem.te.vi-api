50ff3a34ba4259f1a038edd57108b01064b73499
Elineuton Pinheiro <elineuton.ps@gmail.com>
2020-02-15 20:52:44 -0400

    Implementando a autorização

--- src/main/java/com/elineuton/bemtevi/api/config/SecurityConfig.java
+++ src/main/java/com/elineuton/bemtevi/api/config/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.elineuton.bemtevi.api.security.JWTAuthenticationFilter;
+import com.elineuton.bemtevi.api.security.JWTAuthorizationFilter;
 import com.elineuton.bemtevi.api.security.JWTUtil;
 
 @Configuration
@@ -49,6 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
