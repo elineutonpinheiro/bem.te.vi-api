38d5a24bdc4f5f64283e39b8b62cce22ec395c40
Elineuton Pinheiro <elineuton.ps@gmail.com>
2020-02-20 12:57:23 -0400

    Expondo o header location nas respostas

---
+++ src/main/java/com/elineuton/bemtevi/api/filters/HeaderExposureFilter.java
@@ -0,0 +1,24 @@
+package com.elineuton.bemtevi.api.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class HeaderExposureFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		HttpServletResponse res = (HttpServletResponse) response;
+		res.addHeader("access-control-expose-headers", "location");
+		chain.doFilter(request, response);
+		
+	}
+
+}
